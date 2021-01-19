f6a2e48b8d4424e8f9c8b4e2d1ad76233857ca66
werdien jihed <informatique.jihed@gmail.com>
2021-01-19 21:16:22 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import { HashRouter } from "react-router-dom";
 import { Home } from "./Components/home/Home";
+import ReactGA from "react-ga";
 function App() {
+  ReactGA.initialize("G-Z443LGNGNY");
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <HashRouter basename="/">
       <div className="App">
--- src/Components/home/LandingPage.js
+++ src/Components/home/LandingPage.js
@@ -3,6 +3,7 @@ import { Services } from "./services/Services";
 import { Projects } from "./projects/Projects";
 import { Experience } from "./experience/Experience";
 import { Footer } from "./footer/Footer";
+
 export function LeandingPage() {
   return (
     <div>
